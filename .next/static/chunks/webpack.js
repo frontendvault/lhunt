5c4511437f6821d2b818dc7bc42511f8095401f0
manoj pathania <manoj07.pathania@gmail.com>
2024-11-28 17:45:29 +0530

    css updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "681eb9b3272837cf"; }
+/******/ 		__webpack_require__.h = function() { return "ed18cf9822a90a26"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
