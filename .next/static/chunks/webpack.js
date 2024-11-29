27aad085c5b93857be407a35049a147d80e3f8fb
manoj pathania <manoj07.pathania@gmail.com>
2024-11-29 10:00:40 +0530

    carousel arrow

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "23bdd342d020f59d"; }
+/******/ 		__webpack_require__.h = function() { return "e7b32233b0a2333d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
